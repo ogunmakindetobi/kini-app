b7b7b0f27b496a5e3933aa1b68f130abfa8df8a3
toby <toby@tobys-MacBook-Air.local>
2021-11-23 19:02:00 +0000

    timeline

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import logo from "./logo.svg";
+import "./App.css";
+import timelinedata from "./timelinedata.json";
+import react from "react";
+import React, { useState, useEffect } from "react";
+
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import "react-vertical-timeline-component/style.min.css";
+
+function App() {
+  console.log(timelinedata);
+  return (
+    <div className="App">
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+        >
+          <h1>{timelinedata.startTime}</h1>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          name={timelinedata.startTime}
+        ></VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          name={timelinedata.startTime}
+        ></VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          name={timelinedata.startTime}
+        ></VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          name={timelinedata.startTime}
+        ></VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          name={timelinedata.startTime}
+        ></VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
+}
+
+export default App;
